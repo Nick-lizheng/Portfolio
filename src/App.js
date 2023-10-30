bd3f9f6db9a309c3cc9920833abbf9f024c0eca6
nickzheng87 <62922885+nickzheng87@users.noreply.github.com>
2023-10-30 23:28:25 +0800

    update

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
 
 import './App.css';
+import Header from './compoment/header/Header';
+import About from './compoment/about/About';
+import Contact from './compoment/contact/Contact';
+import Experience from './compoment/experience/Experience';
 
-function App() {
+
+
+import React from 'react'
+
+const App = () => {
   return (
-   
-  );
+    <div>
+    <Header />
+    <About />
+    <Contact />
+    <Experience />
+    
+
+    </div>
+    
+
+  )
 }
 
-export default App;
+export default App
