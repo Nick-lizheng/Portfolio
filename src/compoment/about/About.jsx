d139fd857e81b57646e41f0595b3c75c6831b652
nickzheng87 <62922885+nickzheng87@users.noreply.github.com>
2024-03-07 23:41:09 +0800

    editing about section

--- src/compoment/about/About.jsx
+++ src/compoment/about/About.jsx
@@ -1,9 +1,25 @@
-import React from 'react';
+import React from "react";
 import "./about.css";
+import ME from "../../asresst/me.jpg";
+import { FaAward } from "react-icons/fa";
+import { LuUsers } from "react-icons/lu";
+
+
+
 
 const About = () => {
   return (
-    <div>About</div>
+    <section id="about">
+      <h5>Get to Know</h5>
+      <h2>About Me</h2>
+
+      <div className="container about__container">
+        <div>
+          
+        </div>
+
+      </div>
+    </section>
   )
 }
 
