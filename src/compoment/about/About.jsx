6ed94a4768b9fbf09bd59e34173cff4f43190059
nickzheng87 <62922885+nickzheng87@users.noreply.github.com>
2024-03-11 01:32:28 +0800

    finished about

--- src/compoment/about/About.jsx
+++ src/compoment/about/About.jsx
@@ -3,6 +3,7 @@ import "./about.css";
 import ME from "../../asresst/me.jpg";
 import { FaAward } from "react-icons/fa";
 import { LuUsers } from "react-icons/lu";
+import { MdFolderSpecial } from "react-icons/md";
 
 
 
@@ -14,8 +15,39 @@ const About = () => {
       <h2>About Me</h2>
 
       <div className="container about__container">
-        <div>
-          
+        <div className="about_me">
+          <div className="about_me-image">
+            <img src={ME} alt="About Image" />
+          </div>      
+        </div>
+
+        <div className="about__content">
+          <div className="about__cards">
+            <article className="about__card">
+              <FaAward className="about__icon"/>
+              <h5>Experience</h5>
+              <small>5+ Years Working</small>
+            </article>
+
+            <article className="about__card">
+              <LuUsers className="about__icon"/>
+              <h5>Clients</h5>
+              <small>10 + Worldwide</small>
+            </article>
+            
+            <article className="about__card">
+              <MdFolderSpecial className="about__icon"/>
+              <h5>Projects</h5>
+              <small>30+ Completed</small>
+            </article>
+          </div>
+
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quaerat culpa corporis temporibus dolor reprehenderit omnis, laborum cum ad voluptas pariatur itaque voluptates nulla, excepturi ducimus. Suscipit sunt ut illum velit!
+          </p>
+
+          <a href="#contact" className="btn btn-primary">Let's Talk</a>
+
         </div>
 
       </div>
