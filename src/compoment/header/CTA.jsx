a5ba77037c803959281a54cb66a8c7813e7b57b6
nickzheng87 <62922885+nickzheng87@users.noreply.github.com>
2024-03-11 18:07:38 +0800

    add some component

---
+++ src/compoment/header/CTA.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import CV from '../..asresst/Python - Li Zheng.pdf'
+
+export const CTA = () => {
+  return (
+    <div className='cta'>
+        <a href=""></a>
+    </div>
+  )
+}
