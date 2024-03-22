45a6f8767533c1e5e1ccd7240d110be5856510c6
nickzheng87 <62922885+nickzheng87@users.noreply.github.com>
2024-03-22 18:14:46 +0800

    add portfolio

--- src/compoment/portfolio/Portfolio.jsx
+++ src/compoment/portfolio/Portfolio.jsx
@@ -1,9 +1,53 @@
 import React from 'react'
 import "./portfolio.css"
+import PROJECT1 from "../../asresst/project1.jpg"
+
+
+const data = [
+  {
+    id: 1,
+    Image: PROJECT1,
+    title: 'Happsa-waste-capture-portal',
+    github: 'https://github.com',
+    demo: 'https://waste-capture-portal.happsa.com.au/'
+  }
+]
+
+
+
 
 const Portfolio = () => {
   return (
-    <div>Portfolio</div>
+    <section id='portfolio'>
+      <h5>My recent Work</h5>
+      <h2>Portfolio</h2>
+
+      <div className='container portfolio__container'>
+        {
+          data.map(({id, Image, title, github, demo}) => {
+            return (
+              <article key={id} className='portfolio__item'>
+                <div className='portfolio_item-image'>
+                  <img src={Image} alt={title} />
+
+                </div>
+
+                <h3>{title}</h3>
+                <div className='porfolio__item-cta'>
+                  <a href={github} className='btn' target='_blank' rel='noreferrer'>Github</a>
+                  <a href={demo} className='btn btn-primary' target='_blank' rel='noreferrer'>Live Demo</a>
+                </div>
+
+              </article>
+            )
+          }
+
+          )
+        }
+
+      </div>
+
+    </section>
   )
 }
 
--- src/compoment/services/Servics.jsx
+++ src/compoment/services/Servics.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { MdOutlineCheck} from "react-icons/md"
 import "./service.css"
 
 const Servics = () => {
@@ -7,8 +8,76 @@ const Servics = () => {
         <h5>What I Offer</h5>
         <h2>Services</h2>
 
+        <div className="container services__container">
+          <artical className='service'>
+            <div className='service__head'>
+              <h3>Data Visulizasion</h3>
 
-        <div className="container services__container"></div>
+            </div>
+
+            <ul className='service__list'>
+              <li>
+                <MdOutlineCheck className='service__list-icon'/>
+                <p>Creating DashBorad by using QlikSence</p>
+
+              </li>
+              <li>
+                <MdOutlineCheck className='service__list-icon'/>
+                <p>WebScraping data from page</p>
+
+              </li>
+              <li>
+                <MdOutlineCheck className='service__list-icon'/>
+                <p>Handling data with spark and kedro pipeline</p>
+
+              </li>
+              <li>
+                <MdOutlineCheck className='service__list-icon'/>
+                <p>Automation tasking</p>
+
+              </li>
+
+            </ul>
+
+          </artical>
+          {/* end of data visulization */}
+
+          <artical className='service'>
+            <div className='service__head'>
+              <h3>Web Development</h3>
+
+            </div>
+
+            <ul className='service__list'>
+              <li>
+                <MdOutlineCheck className='service__list-icon'/>
+                <p>Buliding FrontEnd</p>
+
+              </li>
+          
+            </ul>
+
+          </artical>
+
+          {/* end of web development */}
+
+          <artical className='service'>
+            <div className='service__head'>
+              <h3>Content creation</h3>
+
+            </div>
+
+            <ul className='service__list'>
+              <li>
+                <MdOutlineCheck className='service__list-icon'/>
+                <p>photoragphy</p>
+
+              </li>
+          
+            </ul>
+
+          </artical>
+        </div>
     </section>
   )
 }
