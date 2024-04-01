8291c3dc86aee1bf053c134d748daf16e68216f2
nickzheng87 <62922885+nickzheng87@users.noreply.github.com>
2024-04-01 10:26:07 +0800

    bug fixed

--- src/compoment/header/Header.jsx
+++ src/compoment/header/Header.jsx
@@ -11,7 +11,7 @@ const Header = () => {
 
     
     <div className='container header__container'>
-      <h5>Hello I,m</h5>
+      <h5>Hello I'm</h5>
       <h1>Nick Li Zheng</h1>
       <h5 className='text-light'>FullStack Developer</h5>
       <CTA/>
